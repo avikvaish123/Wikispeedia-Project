9c591a50f685f715ee6b3daffb2ca8b9833d5d2b
Avik Vaish <avikv2@illinois.edu>
2022-12-12 06:18:57 +0000

    finished commments on kosaraju's file

--- src/kosaraju.hpp
+++ src/kosaraju.hpp
@@ -10,13 +10,20 @@ using namespace std;
 
 class Kosaraju {
 public:
-
+    //constructor
     Kosaraju();
 
-    unordered_map<string, vector<string>> convert(Graph g);
+    //main kosaraju's function that performs a dfs, reverse the graph, and then performs another dfs to return the strongly connected components
+    //Input: Graph object and tronspose of the graph object
+    //Output: Vector of the strongly connected components
     vector<vector<string>> kosaraju(Graph graph, Graph tranposed);
-    void cft(const string& u, unordered_map<string, vector<string>> graph, unordered_map<string, bool>& visited, stack<string>& stack);
-    void dfs(const string& u, const unordered_map<string, vector<string>> graph, unordered_map<string, bool>& visited, vector<string>& scc);
 
 private:
+    //converts our graph structure into a map of vectors so it's easier to traverse through for kosaraju's algorithm
+    unordered_map<string, vector<string>> convert(Graph g);
+    //Computes the finishing order for nodes that are traverse using dfs
+    //puts the nodes on a stack in terms of which dfs finishes first
+    void cft(const string& u, unordered_map<string, vector<string>> graph, unordered_map<string, bool>& visited, stack<string>& stack);
+    //runs dfs on the nodes in the stack and adds the traversed nodes to the strongly connected components
+    void dfs(const string& u, const unordered_map<string, vector<string>> graph, unordered_map<string, bool>& visited, vector<string>& scc);
 };
