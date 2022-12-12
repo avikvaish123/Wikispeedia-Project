643c6bb8d6f6252e6bbe21c0e3d026e1507c58a2
danielgutierrez <daniel.g.2760@gmail.com>
2022-12-12 00:09:54 +0000

    algorithms for medium files

---
+++ build/src/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/brandes.cpp.o"
+  "CMakeFiles/src.dir/brandes.cpp.o.d"
+  "CMakeFiles/src.dir/graph.cpp.o"
+  "CMakeFiles/src.dir/graph.cpp.o.d"
+  "CMakeFiles/src.dir/kosaraju.cpp.o"
+  "CMakeFiles/src.dir/kosaraju.cpp.o.d"
+  "libsrc.a"
+  "libsrc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- entry/main.cpp
+++ entry/main.cpp
@@ -18,24 +18,73 @@ bool sortByVal(const pair<string, int> &a,
 int main()
 {
 
-  // Graph graph("../dataset/articles.tsv", "../dataset/links.tsv");
+  // ifstream fstream("../dataset/links.tsv");
+  // ifstream fstream2("../dataset/articles.tsv");
+  // ofstream outlinks;
+  // ofstream outvert;
+  // outlinks.open("../tests/medium_links.tsv", std::ios_base::app); // append instead of overwrite
+  // outvert.open("../tests/medium_vert.tsv", std::ios_base::app);
 
-  // //initalize edges using adjacency list
-  // graph.initialize_graph();
-  // // graph.printGraph();
+  // string line;
+  // string head_article = "";
+  // int index = 0;
+  // int count = 0;
+  // while (getline(fstream, line)) {
 
-  // vector<Graph::Node*> vertices = graph.getVertices();
+  //   size_t vertex_index = line.find('\t');
+  //   string article = line.substr(0, vertex_index);
+  //   string edge = line.substr(vertex_index + 1);
 
-  // cout << vertices.size() << endl;
+  //   if (article[0] == 'A' && edge[0] == 'A') {
+  //     count++;
+  //     outlinks << line << endl;
+  //   }
+  // }
 
-  // // Brandes brandes;
-  // // map<string, int> mymap = brandes.calculate(graph);
+  // while (getline(fstream2, line)) {
+  //   if (line[0] == 'A') {
+  //     outvert << line << endl;
+  //   }
+  // }
 
-  // map<string, int> mymap = {
-	// 	{"coconut", 10}, {"apple", 5}, {"peach", 350}, {"mango", 8}
-	// };
 
-	// vector<pair<string, int>> vec;
+
+  //Graph graph("../tests/medium_vert.tsv", "../tests/medium_links.tsv");
+  //graph.initialize_graph();
+  //vector<Graph::Node*> vertices = graph.getVertices();
+
+  Graph testGraph("../tests/medium_vert.tsv", "../tests/medium_links.tsv");
+  Graph testGraphTrans("../tests/medium_vert.tsv", "../tests/medium_links.tsv");
+
+  testGraph.initialize_graph();
+  testGraphTrans.transpose_graph();
+
+  Kosaraju k;
+  vector<vector<string>> sccs = k.kosaraju(testGraph, testGraphTrans);
+
+  std::ofstream outfile;
+
+  outfile.open("../tests/Kosaraju.txt", std::ios_base::app); // append instead of overwrite
+
+  for (size_t i = 0; i < sccs.size(); i++) {
+    string line = "{";
+    for (size_t j = 0; j < sccs.at(i).size(); j++) {
+      line += sccs.at(i).at(j) + ", ";
+    }
+    if (line[line.length() - 1] != '{') {
+      line.pop_back();
+      line.pop_back();
+    }
+    line += "}";
+    outfile << line << endl;
+  }
+
+  // Brandes brandes;
+  // map<string, int> mymap = brandes.calculate(graph);
+
+  // cout << "finish calculating brandes" << endl;
+
+  // vector<pair<string, int>> vec;
   // map<string, int> :: iterator it2;
   // for (it2=mymap.begin(); it2!=mymap.end(); it2++) 
   // {
@@ -44,35 +93,19 @@ int main()
 
   // sort(vec.begin(), vec.end(), sortByVal);
 
+  // cout << "finish sorting" << endl;
+
   // std::ofstream outfile;
 
   // outfile.open("../tests/BrandesAlgorithm.txt", std::ios_base::app); // append instead of overwrite
   // for (size_t i = 0; i < vec.size(); i++)
-	// {
-	// 	outfile << vec[i].first << ": " << vec[i].second << endl;
-	// }
+  // {
+  //   outfile << vec[i].first << ": " << vec[i].second << endl;
+  // }
 
-  // Graph testGraph("../tests/kosa_vert.tsv", "../tests/kosa_edges.tsv");
-  // Graph testGraphTrans("../tests/kosa_vert.tsv", "../tests/kosa_edges.tsv");
-
-  Graph testGraph("../dataset/articles.tsv", "../dataset/links.tsv");
-  Graph testGraphTrans("../dataset/articles.tsv", "../dataset/links.tsv");
-
-  testGraph.initialize_graph();
-  testGraphTrans.transpose_graph();
-
-
-  Kosaraju k;
-  vector<vector<string>> sccs = k.kosaraju(testGraph, testGraphTrans);
-
-
-  for (size_t i = 0; i < sccs.size(); i++) {
-    cout << "SCCS: {";
-    for (size_t j = 0; j < sccs.at(i).size(); j++) {
-      cout << sccs.at(i).at(j) << + ", ";
-    }
-    cout << "}" << endl;
-  }
+  // cout << vertices.size() << endl;
+  // graph.printGraph();
+  
 
   return 0;
 }
